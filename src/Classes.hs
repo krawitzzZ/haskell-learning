2ac6953531918ac5f4a08fe88a48b8e7b9ce41a1
Nikita Demin <krawitzzz@hotmail.com>
2020-03-01 19:54:23 +0100

    newtype example

--- src/Classes.hs
+++ src/Classes.hs
@@ -7,8 +7,8 @@ class (Num a) => ExampleClass a where
 data ExampleData a = ExampleData a deriving (Show)
 
 instance Num a => Num (ExampleData a) where
-  (+) (ExampleData x) (ExampleData x') = ExampleData $ x + x
-  (*) (ExampleData x) (ExampleData x') = ExampleData $ x * x
+  (+) (ExampleData x) (ExampleData x') = ExampleData $ x + x'
+  (*) (ExampleData x) (ExampleData x') = ExampleData $ x * x'
   negate (ExampleData x) = ExampleData $ negate x
   abs (ExampleData x) = ExampleData $ abs x
   signum (ExampleData x) = ExampleData $ signum x
@@ -22,3 +22,29 @@ instance ExampleClass Int where
 
 instance ExampleClass Integer where
   lessByTwo x = x - 2
+
+class TooMany a where
+  tooMany :: a -> Bool
+
+instance TooMany Int where
+  tooMany n = n > 42
+
+newtype Goats = Goats Int deriving (Eq, Show)
+
+instance TooMany Goats where
+  tooMany (Goats g) = tooMany g
+
+newtype StrInt = StrInt (Int, String)
+
+newtype IntInt = IntInt (Int, Int)
+
+newtype NumInt a = NumInt (a, a)
+
+instance TooMany StrInt where
+  tooMany (StrInt (i, _)) = tooMany i
+
+instance TooMany IntInt where
+  tooMany (IntInt (i, i')) = tooMany $ i + i'
+
+instance (Num a, TooMany a) => TooMany (NumInt a) where
+  tooMany (NumInt (i, i')) = tooMany $ i + i'
