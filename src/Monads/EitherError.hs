7041bf8eb6129c570a853ea8c464a55d5767bbc5
Nikita Demin <nikita.demin@jsmd-group.com>
2021-09-14 17:14:09 +0200

    either error todo

---
+++ src/Monads/EitherError.hs
@@ -0,0 +1,11 @@
+
+module Monads.EitherError where
+
+
+-- instance (Error e) => Monad (Either e) where
+--   return x = Right x
+--   Right x   >>= f = f x
+--   Left  err >>= f = Left err
+--   fail msg = Left (strMsg msg)
+
+-- TODO: rewrite pole and birds excercise to store the amount of birds on each side
